1acdb1eef866a46e85febd6bbcc9037108c9969c
Vonels <klizka38@gmail.com>
2025-12-11 09:20:13 +0200

    add base

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
 
-import App from "./Components/App/App";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
